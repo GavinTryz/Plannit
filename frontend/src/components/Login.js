3ba722f4db9e150ce881af26489952d6472fb579
José A. Flores Oria <77517759+joseflores01@users.noreply.github.com>
2021-03-27 12:47:30 -0700

    Add files via upload
    Login function is now working

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import {Link } from 'react-router-dom';
 
+
 function Login()
 {
 
@@ -9,33 +10,43 @@ function Login()
 
     const [message,setMessage] = useState('');
 
-    const doLogin = async event =>
+    const doLogin = async event => 
     {
         event.preventDefault();
 
-        // This line is for testing. It'll be removed 
-        alert('doIt() ' + loginName.value + ' ' + loginPassword.value );
-
         var obj = {
-            login:loginName.value,
+            email:loginName.value,
             password:loginPassword.value
         };
         var js = JSON.stringify(obj);
 
-        /*
         try
-        {
-            const response = await fetch(buioldPath('api/login')),
-                {method: 'POST', body:js,headers:{'Content-Type': 'application/json'}});
+        {    
+            const response = await fetch('http://localhost:5000/api/login',
+                {method:'POST',body:js,headers:{'Content-Type': 'application/json'}});
 
             var res = JSON.parse(await response.text());
-            if (res.id <= 0)
+
+            if( res.id <= 0 )
             {
-                setMessage('User password combination incorrect');
+                setMessage('User/Password combination incorrect');
+            }
+            else
+            {
+                var user = {firstName:res.firstName,lastName:res.lastName,id:res.id}
+                localStorage.setItem('user_data', JSON.stringify(user));
+
+                setMessage('');
+                window.location.href = '/dashboard';
             }
         }
-        */
-    }
+        catch(e)
+        {
+            alert(e.toString());
+            return;
+        }    
+    };
+
 
     return(
         <div className = 'backgroundLogin'><br />
@@ -45,12 +56,14 @@ function Login()
                     <input id = 'textField' type="text" id="loginName" placeholder="Username" ref={(c) => loginName = c} /><br />
                     <input id = 'textField' type="password" id="loginPassword" placeholder="Password" ref={(c) => loginPassword = c} /><br />
                     <input type="submit" id="loginButton" class="buttons" value = "Sign In" onClick={doLogin} /><br />
-                    <span className = 'linkTextTitle'>Forgot password?</span>
-                    <Link><a className = 'linkText'>click here</a></Link><br/>
-                    <span className = 'linkTextTitle'>No account?</span>
-                    <Link to ='/createaccount'><a className = 'linkText'>create one!</a></Link>
                 </form>
-                <span id="loginResult">{message}</span>
+                <div id="loginResult">{message}</div>
+                <span className = 'linkTextTitle'>Forgot password?</span>
+                <Link><a className = 'linkText'>click here</a></Link><br/>
+                <span className = 'linkTextTitle'>No account?</span>
+                <Link to ='/createaccount'><a className = 'linkText'>create one!</a></Link>
+                
+                
             </div>
         </div>
 
