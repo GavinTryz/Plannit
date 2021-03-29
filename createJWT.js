a2d5abcaa5797e483cc426071679ee32010ecf50
Iantho1999 <70291326+Iantho1999@users.noreply.github.com>
2021-03-28 20:43:36 -0400

    Add files via upload
    Added JWTs as well as CreateWeek and GetWeek endpoints

---
+++ createJWT.js
@@ -0,0 +1,48 @@
+const jwt = require(jsonwebtoken);
+require("dotenv").config();
+
+exports.createToken = function(fn, ln, id)
+{
+    try
+    {
+        const expiration = new Date();
+        const user = {userId:id, firstName:fn, lastName:ln};
+        const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+    
+        var ret = {accessToken:accessToken};
+    }
+    catch(e)
+    {
+        var ret = {error:e.message};
+    }
+    return ret;
+}
+
+exports.isExpired = function(token)
+{
+    var isError = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, verifiedJWT) =>
+    {
+        if (err)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    });
+
+    return isError;
+}
+
+exports.refresh = function(token)
+{
+    var ud = jwt.decode(token, {complete:true});
+
+    var userId = ud.payload.id;
+    var firstName = ud.payload.firstName;
+    var lastName = ud.payload.lastName;
+
+    return createToken(firstName, lastName, userId)
+}
--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const MongoClient = require('mongodb').MongoClient;
 const url = 'mongodb+srv://root:7eW0JKa3jcmQNXot@cluster0.abrob.mongodb.net/Plannit?retryWrites=true&w=majority';
 const client = new MongoClient(url);
 client.connect();
+
 const mongoose = require('mongoose');
+const jwt = require('./createJWT');
 
 const cors = require('cors');
 const app = express();
@@ -33,6 +35,8 @@ app.post('/api/login', async (req, res, next) => {
     var lastname = '';
     var error = '';
 
+    var ret;
+
 
     if (results.length > 0)
     {
@@ -46,6 +50,15 @@ app.post('/api/login', async (req, res, next) => {
             userID = body._id;
             firstname = body.firstname;
             lastname = body.lastname;
+
+            try
+            {
+                ret = jwt.createToken(firstname, lastname, userID);
+            }
+            catch(e)
+            {
+                ret = {error:e.message};
+            }
         }
         
     }
@@ -55,13 +68,17 @@ app.post('/api/login', async (req, res, next) => {
     }
 
     
-    res.json({firstname: firstname, lastname: lastname, userID: userID, error: error});
+    // res.json({firstname: firstname, lastname: lastname, userID: userID, error: error});
+    res.status(200).json(ret);
 })
 
 app.post('/api/register', async (req, res, next) => {
     const {firstname, lastname, email, password} = req.body;
     const db = client.db();
     var error = '';
+
+    var ret;
+
     const results = await(db.collection('Users').find( {email : email} )).toArray();
     
     if (results.length > 0)
@@ -82,13 +99,80 @@ app.post('/api/register', async (req, res, next) => {
                 password: password,
                 verified: false}
             ]);
+            
+            try
+            {
+                ret = jwt.createToken(firstname, lastname, userID);
+            }
+            catch(e)
+            {
+                ret = {error:e.message};
+            }
         }    
         catch(e)
         {
-            error = e.toString();
+            ret = {error:e.message};
         }
-        res.status(200).json({error: error});
+        res.status(200).json(ret);
     }
     
 })
+
+app.post('/api/createWeek', async (req, res, next) => {
+    const db = client.db();
+    const {day, startTime, endTime, title, userID} = req.body;
+
+    try
+    {
+        db.collection('Blocks').insertMany([ 
+            {day: day,
+             startTime: startTime,
+             endTime: endTime,
+             title: title,
+             userID: userID}
+        ]);
+            
+        try
+        {
+            ret = jwt.createToken(day, startTime, endTime, title, userID);
+        }
+        catch(e)
+        {
+            ret = {error:e.message};
+        }
+    }    
+    catch(e)
+    {
+        ret = {error:e.message};
+    }
+    res.status(200).json(ret);
+});
+
+app.post('/api/getWeek', async (req, res, next) => {
+    const db = client.db();
+    const {userID} = req.body;
+
+    try
+    {
+        db.collection('Blocks').find( 
+            {userID: userID},
+            {_id:0, day:1, startTime:1, endTime:1, title:1}
+        );
+            
+        try
+        {
+            ret = jwt.createToken(day, startTime, endTime, title);
+        }
+        catch(e)
+        {
+            ret = {error:e.message};
+        }
+    }    
+    catch(e)
+    {
+        ret = {error:e.message};
+    }
+    res.status(200).json(ret);
+});
+
 app.listen(5000); // start Node + Express server on port 5000
