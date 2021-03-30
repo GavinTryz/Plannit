ad908b3f0bae018cc22e84f3d0e6890085e2b7c8
jessienguyen32 <77517623+jessienguyen32@users.noreply.github.com>
2021-03-30 15:48:12 -0500

    Update login.js

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -1,1 +1,71 @@
+import React from 'react';
 
+//do npm install redux react-redux
+
+function Login()
+{
+  var obj = {login:loginName.value, password:loginPassword.value};
+  var js = JSON.stringify(obj);
+ 
+  try
+  {
+    const response = await fetch(bp.buildPath('api/login'), //EDIT FILE NAME
+         {method:'POST', body:js, headers:{'Content-Type' : 'application/json'}});
+   
+    var res = JSON.parse(await response.text());
+   
+    if( res.id <= 0 )
+    {
+      setMessage('User/Password combination incorrect');
+    }
+    else
+    {
+      var user = {firstName:res.firstName, lastName:res.lastName, id:res.id}
+      localStorage.setItem('user_data', JSON.stringify(user));
+     
+      setMessage('');
+      window.location.href = ''; //SET NEW PAGE LOCATION
+    }
+  }
+
+
+    /*var loginName;
+    var loginPassword;
+
+    const doLogin = async event =>
+    {
+        event.preventDefault();
+
+        var obj = {
+            login:loginName.value,
+            password:loginPassword.value
+        };
+        var js = JSON.stringify(obj);
+
+
+        alert('yes it is working!');
+    }*/
+    
+    
+    return(
+        <div><br />
+
+        <div className= 'loginSection'>
+            <span>Plannit Sign In</span>
+            <form onSubmit = {doLogin}>
+                <input type="text" id="loginName" placeholder="Username" /><br />
+                <input type="password" id="loginPassword" placeholder="Password" /><br />
+                <input type="submit" id="loginButton" class="buttons" value = "Sign In" onClick={doLogin} /><br />
+                <span>Forgot username/password?</span>
+                <a>click here</a><br/>
+                <span>No account?</span>
+                <a>create one!</a>
+            </form>
+            <span id="loginResult"></span>
+        </div>
+
+        </div>
+
+    );
+}
+export default Login;
