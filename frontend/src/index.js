b8238e3706e939fd692034e6517b25c8e666352e
jessienguyen32 <77517623+jessienguyen32@users.noreply.github.com>
2021-03-30 14:41:26 -0500

    Example case of redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,13 +4,36 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+/*
+import {createStore} from 'redux';
+
 //STORE -> GLOBALIZED STATE
 
 //ACTION (just describes action)
+//just returns an object eg.
+const incrememt = () => {
+  return {
+      type: 'INCREMENT'
+      }
+}
 
 //REDUCER (how action modifies storage)
 
+const counter = (state = 0, action) => {
+ switch(action.type){
+   case "INCREMENT":   
+      return state + 1; //for dec, state - 1
+ }
+}
+
+let store = createStore(counter)
+
 //DISPATCH (executes action)
+
+store.dispatch(increment());  //1
+store.dispatch(decrement());  //0
+store.dispatch(decrement());  //-1
+*/
 
 ReactDOM.render(
   <React.StrictMode>
