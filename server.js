291de51255daf5c49c736035eb670d774ed934bd
Ben <benjamindbraun@gmail.com>
2021-03-31 20:19:02 -0400

    debugging

--- server.js
+++ server.js
@@ -28,11 +28,11 @@ app.use((req, res, next) => {
 if (process.env.NODE_ENV === 'production') 
 {
   // Set static folder
-  app.use(express.static(path.join(__dirname, '../frontend', 'build')));
+  app.use(express.static(path.join(__dirname, 'frontend', 'build')));
 
   app.get('*', (req, res) => 
   {
-    res.sendFile(path.join(__dirname, '../frontend', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
   });
 }
 
