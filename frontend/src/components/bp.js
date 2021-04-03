d782ae1c792c27d5a31f56892d8f138bcc51c873
jessienguyen32 <77517623+jessienguyen32@users.noreply.github.com>
2021-04-03 14:47:57 -0500

    Error check

--- frontend/src/components/bp.js
+++ frontend/src/components/bp.js
@@ -2,13 +2,13 @@ exports.buildPath = function (route)
 {
     const app_name = 'plannit-cop4331';
 
-    if (process.env.Node_ENV === 'production')
-    {
+    //if (process.env.Node_ENV === 'production')
+    //{
         return 'https://' + app_name + '.herokuapp.com/' + route;
-    }
+    /*}
     else
     {
         return 'https://localhost:5000/' + route;
-    }
+    }*/
 
 }
