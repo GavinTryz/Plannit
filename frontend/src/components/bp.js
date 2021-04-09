6f76a1ed1e9f4930c5cdf495f8a15bf14aa149c1
José A. Flores Oria <77517759+joseflores01@users.noreply.github.com>
2021-04-09 01:36:39 -0400

    Update bp.js

--- frontend/src/components/bp.js
+++ frontend/src/components/bp.js
@@ -2,7 +2,7 @@ exports.buildPath = function (route)
 {
     const app_name = 'plannit-cop4331';
 
-    if (process.env.Node_ENV === 'production')
+    if (process.env.NODE_ENV === 'production')
     {
         return 'https://' + app_name + '.herokuapp.com/' + route;
     }
