3dc2d1cb206e45ecc213bdd876d10aa5fac93dd7
Ben <benjamindbraun@gmail.com>
2021-04-24 01:48:25 -0400

    added verifyemail landing page in react

---
+++ frontend/src/pages/VerifyEmailPage.js
@@ -0,0 +1,34 @@
+// pass daysAvailabble: sunday -> saturday true or false
+// pass time: array of integers (0-23)
+import React, {useState, useEffect} from 'react';
+import {useLocation, useHistory} from 'react-router-dom';
+import axios from 'axios';
+import queryString from 'query-string';
+
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+
+export default function InviteUserPage(){
+    const [error, setError] = useState("");
+    const {search} = useLocation();
+    const values = queryString.parse(search);
+    const bp = require('../components/bp.js');
+    let history = useHistory();
+    useEffect(() => {
+        axios.post(bp.buildPath('api/verifyEmail'), {token: values.token})
+        .then((res) => {
+            history.push("/")
+        })
+        .catch((error) => {
+            setError("Error: please retry");
+        });
+    }, []);
+
+    return(
+        <h1>
+            {loading &&
+                error
+            }
+        </h1>
+        );
+}
