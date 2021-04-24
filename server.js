a002e19688e0050c4a9e87c6ad610d2238899d3f
Ben <benjamindbraun@gmail.com>
2021-04-24 01:22:45 -0400

    more console.logs

--- server.js
+++ server.js
@@ -173,11 +173,14 @@ app.post('/api/register', async (req, res, next) => {
 });
 
 app.get('/verifyEmail', async(req, res, next) => {
+    console.log("in verify")
     var error = '';
     const db = client.db();
     const {token} = req.query;
+    console.log("token: " + token);
     try
     {
+        console.log("in try");
         const email = jwtLib.verify(token, process.env.SENDGRID_API_KEY);
         console.log(`email ${email}`);
         var user = await db.collection('Users').findOne({email: email.email}, {_id:0, verified:1});
@@ -199,7 +202,7 @@ app.get('/verifyEmail', async(req, res, next) => {
         console.log(error);
         return res.status(200).json({error: error});
     }
-
+    console.log("returning");
     return res.status(200).json({error: error});
 });
 
