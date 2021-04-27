33a624f3c50cad780cebdd703d4cf1cc4487b51b
jessienguyen32 <77517623+jessienguyen32@users.noreply.github.com>
2021-04-27 15:08:00 -0500

    Clearing Code

--- frontend/src/components/SideBar.js
+++ frontend/src/components/SideBar.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import RetrieveEvent from './RetrieveEvent';
 import SetWeekButton from './SetWeekButton';
-import ViewEventButton from './ViewEventButton';
-import TypicalWeekButton from './TypicalWeekButton';
 import './SideBar.css';
-
-import ParticipantList from './ParticipantList';
-import EMPTY_BUTTON from './EMPTY_BUTTON';
 
 function SideBar()
 {
@@ -15,10 +10,6 @@ function SideBar()
             <p className="Sidebar"><br/><br/><br/>
             <RetrieveEvent />
             <SetWeekButton />
-            <ViewEventButton />
-            <TypicalWeekButton />
-            <EMPTY_BUTTON />
-            <ParticipantList />
             </p>
         </div>
     );
