824170f47e7b9d35f7fedda2fd62a2f8be0a6ffa
José A. Flores Oria <77517759+joseflores01@users.noreply.github.com>
2021-04-27 01:19:58 -0400

    Update ViewEventsPage.js

--- frontend/src/pages/ViewEventsPage.js
+++ frontend/src/pages/ViewEventsPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import InternalMenuBar from '../components/InternalMenuBar';
 import SideBar from '../components/SideBar';
-import './viewEventsPageStyles.css;
+import './viewEventsPageStyles.css';
 import MainViewEvents from '../components/MainViewEvents';
 
 const ViewEventsPage = () =>
