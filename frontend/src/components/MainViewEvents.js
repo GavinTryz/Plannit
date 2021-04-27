5b0b518dd4f9a546824aac55fb691bb27342d53f
Ben <benjamindbraun@gmail.com>
2021-04-27 19:30:55 -0400

    fixed view events issue

--- frontend/src/components/MainViewEvents.js
+++ frontend/src/components/MainViewEvents.js
@@ -8,12 +8,20 @@ import axios from 'axios';
 
 function MainViewEvents()
 {
+    const [loading, setLoading] = useState(true);
+    const [availability, setAvailability] = useState([[]]);
     const eventData = useSelector(state => state.eventData);
+    useEffect(() => {
+        setAvailability(calculateAvailability());
+        while(!availability)
+        {
 
+        }
+        setLoading(false);
+    }, [])
     function calculateAvailability() {
         const rows = 7;
         const cols = 48;
-
         var availabilityTable = Array.from({ length: rows }, () => 
         Array.from({ length: cols }, () => 0)
         );
@@ -35,8 +43,11 @@ function MainViewEvents()
     }
     var timeArr = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
     return(
-        <div class="main">
-            <span>{eventData.eventName}</span>
+        <div>
+        { 
+            !loading &&
+            <div class="main">
+                <span>{eventData.eventName}</span>
                 <InvitationPopover />
                 <EventCalendar 
                     daysAvailable={eventData.daysOfWeek}
@@ -44,6 +55,9 @@ function MainViewEvents()
                     calendar={calculateAvailability()}
                     numParticipants={eventData.participants.length}
                 /> 
+            </div>
+        
+        }
         </div>
     );
 }
--- frontend/src/pages/HomePage.test.js
+++ frontend/src/pages/HomePage.test.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import {configure, mount} from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
-
+import {MemoryRouter} from 'react-router-dom';
 import HomePage from './HomePage';
 import MenuBar from '../components/MenuBar';
-import CreateAccount from '../components/CreateAccount';
+import CreateAccountButton from '../components/CreateAccountButton';
 
 configure({adapter: new Adapter()});
 
 describe('testing home page components all render', () => {
-    let wrapper = mount(<HomePage />);
+    global.window = { location: { pathname: null } };
+    let wrapper = mount(<MemoryRouter><HomePage /></MemoryRouter>);
     test('menu bar loads', () => {
         expect(wrapper.find(MenuBar)).toHaveLength(1);
     });
 
     test('menu bar loads', () => {
-        expect(wrapper.find(CreateAccount)).toHaveLength(1);
+        expect(wrapper.find(CreateAccountButton)).toHaveLength(1);
+    });
+
+    test('create account button changes pages', () => {
+        wrapper.find('.createAccountButton').simulate('click');
+        expect(global.window.location.pathname).toEqual('/createaccount');
     });
 })
