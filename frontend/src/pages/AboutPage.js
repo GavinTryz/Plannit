ca45c79f1eff570484ab3fba34bced8c20ca86d3
Iantho1999 <70291326+Iantho1999@users.noreply.github.com>
2021-04-27 23:26:42 -0400

    Added About Page

--- frontend/src/pages/AboutPage.js
+++ frontend/src/pages/AboutPage.js
@@ -5,10 +5,43 @@ import MenuBar from '../components/MenuBar';
 
 const AboutPage = () =>
 {
+  const background = 
+  {
+      backgroundImage: 'url(/graphicplanet.png)',
+      backgroundPosition: 'center',
+      width: '100vw',
+      height: '100vh',
+      backgroundSize: 'cover'
+  }
+
+  const textStyle = 
+  {
+      color: 'white',
+      fontFamily: 'helvetica',
+      fontSize: '18pt',
+      textAlign: 'center',
+      padding: '50px'
+  }
+
+  const listStyle = 
+  {
+    listStyle: 'none'
+  }
 
     return(
-      <div>
+      <div style = {background}>
         <MenuBar />
+        <p style = {textStyle}>
+
+        <h1>What is Plannit?</h1>
+          Plannit is a tool for planning and organizing events with groups of people.
+          <h2>Features</h2>
+          <ul style = {listStyle}>
+            <li>Create and customize events spanning across a week.</li><br></br>
+            <li>Set a default week, based on your typical schedule.</li><br></br>
+            <li>Find a time that works best for everyone.</li><br></br>
+          </ul>
+          </p>
       </div>
     );
 };
